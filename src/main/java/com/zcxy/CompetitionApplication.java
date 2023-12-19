4e8944b483c2a87134246d92be3bbf0be3bd45b1
Horace Mac <1287039108@qq.com>
2023-12-20 00:06:27 +0800

    file

---
+++ src/main/java/com/zcxy/CompetitionApplication.java
@@ -0,0 +1,16 @@
+package com.zcxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Horace Leoi
+ * @date : 2023/12/20
+ * @remark :
+ */
+@SpringBootApplication
+public class CompetitionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CompetitionApplication.class, args);
+    }
+}
