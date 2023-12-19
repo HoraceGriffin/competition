d2e7b5d8f74f4f9793f56d1f90042ce0a6e056dc
“1985598868” <1985598868@qq.com>
2023-12-19 20:32:45 +0800

    login

---
+++ src/main/java/com/zcxy/controller/LoginController.java
@@ -0,0 +1,75 @@
+package com.zcxy.controller;
+
+
+
+import com.zcxy.entity.Admin;
+import com.zcxy.service.AdminService;
+import com.zcxy.utils.MD5Util;
+import com.zcxy.utils.ResultUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.text.ParseException;
+
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private AdminService adminService;
+
+    /**
+     * 登录
+     */
+    @RequestMapping("/login")
+    @ResponseBody
+    public ResultUtil login(String username, String password, HttpServletRequest request, HttpSession session)
+            throws ParseException {
+        Admin admin1 = adminService.getAdminByUsername(username);
+        if (null != admin1) {
+            Admin admin = adminService.login(username, MD5Util.MD5Encode(password));
+            if (null != admin) {
+                session.setAttribute("admin", admin);
+                return ResultUtil.ok(admin);
+            } else {
+                return ResultUtil.error("密码错误！");
+            }
+        } else {
+            return ResultUtil.error("账号不存在！");
+        }
+    }
+
+    @RequestMapping("/loginOut")
+    public ModelAndView toLogout(HttpServletRequest request,HttpSession session) throws ParseException {
+
+        return new ModelAndView("login");
+    }
+
+    /************************************页面跳转******************************/
+
+    @RequestMapping("/content")
+    public ModelAndView Main() {
+        return new ModelAndView("admin/main");
+    }
+
+    @RequestMapping("/admin")
+    public ModelAndView toAdminIndex() {
+        return new ModelAndView("admin/index");
+    }
+
+    @RequestMapping("/supperAdmin")
+    public ModelAndView supperAdmin() {
+        return new ModelAndView("superadmin/index");
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        return new ModelAndView("login");
+    }
+}
---
+++ src/main/java/com/zcxy/service/AdminService.java
@@ -0,0 +1,52 @@
+package com.zcxy.service;
+
+import com.zcxy.entity.Admin;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper
+public interface AdminService {
+
+    /**
+     * 登录相关
+     */
+    @Select("select * from tb_admin where username=#{username} and password=#{password}")
+    Admin login(@Param("username") String username, @Param("password") String password);
+
+
+    /**
+     * 查看管理员个人信息
+     */
+    @Select("select * from tb_admin where id=#{id}")
+    Admin getAdminById(@Param("id") Integer id);
+
+    /**
+     * 修改管理员个人信息
+     */
+    @Update("update tb_admin set nickname=#{nickname},sex=#{sex},phone=#{phone},email=#{email} where id=#{id}")
+    int updateAdmin(Admin admin);
+
+    /**
+     * 查看管理员信息
+     */
+    @Select("select * from tb_admin where username=#{username}")
+    Admin getAdminByUsername(@Param("username") String username);
+
+    /**
+     * 修改管理员密码
+     */
+    @Update("update tb_admin set password=#{password} where id=#{id}")
+    void updatePassword(Admin admin);
+
+    /**
+     * 上传头像信息
+     */
+    @Update("update tb_admin set image=#{image} where id=#{id}")
+    void uploadPhoto(Admin admin);
+
+
+}
